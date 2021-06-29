73f7d830900eca1d2620c157f4641ab097ab2a7c
wendleypf <wendleypf@gmail.com>
2021-06-28 21:31:59 -0300

    feat: integration bulma

---
+++ integration-bulma/webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * @author wendleypf <wendleypf@gmail.com>
+ */
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
+}
